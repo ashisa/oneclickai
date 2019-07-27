bac88509d8cfda89db1f510e3cb2a8bc8ca7326d
Ashish Sahu <ashisa@users.noreply.github.com>
2019-07-27 21:50:03 +0530

    Delete localsettings.cs

--- localsettings.cs
+++
@@ -1,8 +0,0 @@
-namespace oneclickai
-{
-    class localsettings
-    {
-        public static string cognitive_service_key = "74ac189d5eb84f5d883d1fc2c77f3fa7";
-        public static string cognitive_service_endpoint = "https://centralindia.api.cognitive.microsoft.com/";		
-    }
-}
